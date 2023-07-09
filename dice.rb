f0f2ffcd9c8fe04322530a6984333f5707a09439
jbenzer <136658052+jbenzer@users.noreply.github.com>
2023-07-09 23:13:57 +0000

    upadtesss

--- dice.rb
+++ dice.rb
@@ -1,8 +1,24 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "Hello World"
+  #"Hello World"
+  "
+  <h1>Dice Roll</h1>
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-side dice</a></li>
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+  </ul>
+  "
+
 end
 
 get("/zebra") do
@@ -58,4 +74,3 @@ get("/dice/5/4") do
   "<h1>5d4</h1>
   <p>#{outcome_d4}</p>"
 end
-
